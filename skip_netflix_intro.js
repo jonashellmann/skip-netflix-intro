d38d6ec7aab12900574ee00d0208571eaf7bb387
Jonas Hellmann <hellmann.jonas@web.de>
2020-02-02 17:12:47 +0100

    Implement auto-skipping Netflix intro

---
+++ skip_netflix_intro.js
@@ -0,0 +1,19 @@
+// Create observer with callback function
+const callback = function(mutations, observer) {
+	// Iterate over every DOM mutation
+	for (let mutation of mutations) {
+		// Iterate over all added nodes in a mutation
+		for (let node of mutation.addedNodes) {
+			// Click on node if it
+			if (node.matches('.skip-credits')) {
+				node.firstChild.click();
+			}
+		}
+	}
+};	
+const mutationObserver = new MutationObserver(callback);
+
+// Start oberserving every change in the DOM
+const element = document.documentElement;
+const config = { attributes: true, childList: true, subtree: true };
+mutationObserver.observe(element, config);
