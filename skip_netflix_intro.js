a662bce3ff208b9d6cc3a6313428a542849d4042
Jonas Hellmann <hellmann.jonas@web.de>
2022-01-22 12:10:10 +0100

    Fix not working click on button

--- skip_netflix_intro.js
+++ skip_netflix_intro.js
@@ -1,19 +1,12 @@
-// Create observer with callback function
 const callback = function (mutations, observer) {
-    // Iterate over every DOM mutation
     for (let mutation of mutations) {
-        // Iterate over all added nodes in a mutation
         for (let node of mutation.addedNodes) {
-            // Click on node if it
-            if (node.matches('.watch-video--skip-content')) {
-                node.firstChild.click();
-            }
+            node.querySelector("button.watch-video--skip-content-button")?.firstChild.click();
         }
     }
 };
 const mutationObserver = new MutationObserver(callback);
 
-// Start oberserving every change in the DOM
 const element = document.documentElement;
 const config = { attributes: true, childList: true, subtree: true };
 mutationObserver.observe(element, config);
